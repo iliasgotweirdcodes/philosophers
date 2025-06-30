e56ed134162124b32fb8a3007074509d13d45591
ilel-hla <iliaselhal1@gmail.com>
2025-06-30 18:17:11 +0100

    everything works fine

--- philo/philo_utils.c
+++ philo/philo_utils.c
@@ -6,28 +6,26 @@
 /*   By: ilel-hla <ilel-hla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 23:57:17 by ilel-hla          #+#    #+#             */
-/*   Updated: 2025/06/30 17:39:35 by ilel-hla         ###   ########.fr       */
+/*   Updated: 2025/06/30 18:17:05 by ilel-hla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int create_philo_threads(t_table *table)
+int	create_philo_threads(t_table *table)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < table->num_philos)
-    {
-        if (pthread_create(&table->philo[i].thread, NULL, philo_routine, &table->philo[i]) != 0)
-            ft_error(ERR_THREAD);
-        i++;
-    }
-
-    if (pthread_create(&table->monitor, NULL, simulation_monitor, table) != 0)
-        ft_error(ERR_THREAD);
-
-    return (1); // Return success
+	i = 0;
+	while (i < table->num_philos)
+	{
+		if (pthread_create(&table->philo[i].thread, NULL, philo_routine, &table->philo[i]) != 0)
+			ft_error(ERR_THREAD);
+		i++;
+	}
+	if (pthread_create(&table->monitor, NULL, simulation_monitor, table) != 0)
+		ft_error(ERR_THREAD);
+	return (1);
 }
 
 void	join_threads(t_table *table)
